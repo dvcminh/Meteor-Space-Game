c7239248a953790127d729c8ac52863a86791c6e
Vũ Đức Minh <21522348@ms.uit.edu.vn>
2022-11-18 15:59:21 +0700

    Add project files.

---
+++ Bida/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Bida
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {            
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.pictureBox1.BackgroundImage = global::Bida.Properties.Resources.spacescraft;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(309, 313);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 60);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = global::Bida.Properties.Resources.space_background_with_landscape_alien_planet_c_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(717, 413);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Bida/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
+
+namespace Bida
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 50;
+            timer1.Start();
+
+            timer2.Interval = 50;
+            timer2.Start();
+        }
+        Point p = new Point(0, 0);
+        List<cRectangle> cc = new List<cRectangle>();
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(var shape in cc)
+            {
+                shape.draw(e.Graphics);
+            }            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (var shape in cc)
+            {
+                shape.y += 5;
+            }
+            this.Invalidate();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random Random = new Random();
+
+            p.X = Random.Next(1000);
+            p.Y = 50;
+            cRectangle rect = new cRectangle(p.X, p.Y, 50, 50);
+
+            cc.Add(rect);
+
+            this.Invalidate();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Image image = Image.FromFile("D:\\Môn học\\Năm 2\\HK 1\\Lập trình trực quan\\BT lớp\\18-11-2022\\Bida\\Bida\\Resources\\spiked ship 3.PNG");
+            Graphics g = e.Graphics;
+            Rectangle rect = new Rectangle(10, 10, 20, 20);
+
+            g.DrawImage(image, rect);
+
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+
+            if (e.KeyCode == Keys.D) x += 10;
+            else if (e.KeyCode == Keys.A) x -= 10;
+            else if (e.KeyCode == Keys.W) y -= 10;
+            else if (e.KeyCode == Keys.S) y += 10;
+            else if (e.KeyCode == Keys.Space)
+            {
+                /*
+                Image image = Image.FromFile("D:\\Môn học\\Năm 2\\HK 1\\Lập trình trực quan\\BT lớp\\18-11-2022\\Bida\\Bida\\Resources\\meteor_PNG22.png");
+                Rectangle rect = new Rectangle(x, y+10, image.Width, image.Height);
+                g.DrawImage(image, rect);*/
+            }
+
+            pictureBox1.Location = new Point(x, y);
+        }
+
+        
+    }
+    public class cRectangle
+    {
+        public int x;
+        public int y;
+        public int a;
+        public int b;
+
+        public cRectangle(int x, int y, int a, int b)
+        {
+            this.x = x;
+            this.y = y;
+            this.a = a;
+            this.b = b; 
+        }
+
+        public void draw(Graphics g)
+        {
+            Rectangle rect = new Rectangle(this.x,this.y,this.a,this.b);
+            Image image = Image.FromFile("D:\\Môn học\\Năm 2\\HK 1\\Lập trình trực quan\\BT lớp\\18-11-2022\\Bida\\Bida\\Resources\\laserBullet.png");
+            g.DrawImage(image, rect);
+        }     
+    }
+
+    public class SpaceShip
+    {
+        int x;
+        int y;
+    }
+}
